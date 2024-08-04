c92b7dc9afca3f8139ef4a25ee998b0c4437c5e3
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-04 20:40:42 +0630

    Route Group

---
+++ app/(userprofile)/preferences/page.tsx
@@ -0,0 +1,7 @@
+export default function PreferencesPage() {
+  return (
+    <main>
+      <h1>Preferences</h1>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
   return (
     <main>
       <h1>Welcome To Next.js</h1>
-      <Image src="./vercel.svg" alt="Vercel Logo" width={73} height={16} />
     </main>
   );
 }
