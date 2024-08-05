7d0e5d1dfc03db3d0a7e090f54c47f412957a43b
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-06 00:29:55 +0630

    Add:Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,33 @@
-import { cookies } from "next/headers";
+// import { cookies } from "next/headers";
+
+import createPost from "@/server/actions/create-post";
+import getPosts from "@/server/actions/get-post";
 
 export default async function Home() {
-  cookies();
+  // cookies();
+  const { error, success } = await getPosts();
+
+  if (error) {
+    throw new Error(error);
+  }
+
   return (
     <main>
       <div>{Date.now()}</div>
+      <h1 className="bg-red-600 text-white text-3xl">Your Posts</h1>
+      {success ? (
+        success.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+          </div>
+        ))
+      ) : (
+        <h1>Hello</h1>
+      )}
+      <form action={createPost} className="bg-green-600 p-2 mx-auto">
+        <input type="text" name="title" className="bg-black" required />
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 }
