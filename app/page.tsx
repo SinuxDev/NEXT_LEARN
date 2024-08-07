652ea0a735c723add2603ed0287dd3eeb0becb6e
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-07 22:37:33 +0630

    Remove(Components): [Button&Loader2]

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
-
 export default async function Home() {
   return (
     <main className="text-4xl">
       <h1>Hello There </h1>
-      <Button variant="destructive">
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please Wait
-      </Button>
     </main>
   );
 }
