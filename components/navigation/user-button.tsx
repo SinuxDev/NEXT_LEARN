bb6327700df66f287039972432e3d7fd3486a31c
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-08 16:40:37 +0630

    add:Avatar for user default profile in nav bar

--- components/navigation/user-button.tsx
+++ components/navigation/user-button.tsx
@@ -2,11 +2,17 @@
 
 import { Session } from "next-auth";
 import { signOut } from "next-auth/react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 export default function UserButton({ user }: Session) {
   return (
-    <div>
-      <h1>{user?.name}</h1>
+    <div className="flex justify-evenly px-2 w-44 items-center">
+      <Avatar className="cursor-pointer">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      {/* sign out button is only for now  */}
       <button onClick={() => signOut()}>sign out</button>
     </div>
   );
