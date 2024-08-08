6fb805a0f8f0e9d19176f6743fe2ae2b191b6ccd
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-09 03:03:34 +0630

    add(Types): RegisterSchema

--- types/types.ts
+++ types/types.ts
@@ -9,3 +9,15 @@ export const LoginSchema = z.object({
   }),
   code: z.optional(z.string()),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Invalid email address",
+  }),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters long",
+  }),
+  name: z.string().min(4, {
+    message: "Name must be at least 4 characters long",
+  }),
+});
