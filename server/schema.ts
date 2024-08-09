16297494af37da774611445840638830eb9fc1ac
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-09 23:31:36 +0630

    update(Schema): Due to requirement

--- server/schema.ts
+++ server/schema.ts
@@ -8,17 +8,19 @@ import {
   timestamp,
 } from "drizzle-orm/pg-core";
 import type { AdapterAccount } from "next-auth/adapters";
+import { createId } from "@paralleldrive/cuid2";
 
 export const RoleEnum = pgEnum("roles", ["user", "admin"]);
 
 export const users = pgTable("user", {
   id: text("id")
     .primaryKey()
-    .$default(() => crypto.randomUUID()),
+    .$defaultFn(() => createId()),
   name: text("name"),
   email: text("email").unique(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
+  password: text("password"),
   twoFactorEnabled: boolean("twoFactorEnabled").default(false),
   role: RoleEnum("roles").default("user"),
 });
@@ -50,9 +52,12 @@ export const accounts = pgTable(
 export const email_verificationTokens = pgTable(
   "email_verificationToken",
   {
-    identifier: text("id").notNull(),
+    identifier: text("id")
+      .notNull()
+      .$defaultFn(() => createId()),
     token: text("token").notNull(),
     expires: timestamp("expires", { mode: "date" }).notNull(),
+    email: text("email").unique().notNull(),
   },
   (verificationToken) => ({
     compositePk: primaryKey({
