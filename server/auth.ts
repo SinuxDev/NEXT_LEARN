85987c37bcddaeff74f2b3a9935c415708a6b571
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-10 21:48:49 +0630

    config(Auth) : auth.ts [ Credentials]

--- server/auth.ts
+++ server/auth.ts
@@ -1,8 +1,13 @@
-import NextAuth from "next-auth";
+import NextAuth, { CredentialsSignin } from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/server";
 import Google from "@auth/core/providers/google";
 import GitHub from "@auth/core/providers/github";
+import Credentials from "next-auth/providers/credentials";
+import { LoginSchema } from "@/types/types";
+import { eq } from "drizzle-orm";
+import { users } from "./schema";
+import bcrypt from "bcrypt";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -19,6 +24,34 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GitHub({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validateFields = LoginSchema.safeParse(credentials);
+
+        if (validateFields.success) {
+          const { email, password } = validateFields.data;
+
+          const userDoc = await db.query.users.findFirst({
+            where: eq(users.email, email),
+          });
+
+          if (!userDoc || !userDoc.password) {
+            return null;
+          }
+
+          const isPasswordMatch = await bcrypt.compare(
+            password,
+            userDoc.password
+          );
+
+          if (isPasswordMatch) {
+            return userDoc;
+          }
+        }
+
+        return null;
+      },
     }),
   ],
 });
