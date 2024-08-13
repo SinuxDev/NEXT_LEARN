ae93499ba0dd285e87fcd6da1314012acf12fa1b
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-14 02:32:34 +0630

    create : server actions  for products creattion

---
+++ server/dashboard/create-products.tsx
@@ -0,0 +1,53 @@
+"use server";
+import { createSafeActionClient } from "next-safe-action";
+import { products } from "../schema";
+import { ProductSchema } from "@/types/types";
+import { db } from "..";
+import { eq } from "drizzle-orm";
+
+const action = createSafeActionClient();
+
+export const createProducts = action
+  .schema(ProductSchema)
+  .action(async ({ parsedInput }) => {
+    try {
+      const { id, description, title, price } = parsedInput;
+
+      if (id) {
+        const currentProducts = await db.query.products.findFirst({
+          where: eq(products.id, id),
+        });
+
+        if (!currentProducts) {
+          return { error: "Product not found" };
+        }
+
+        const updateProduct = await db
+          .update(products)
+          .set({
+            description,
+            title,
+            price,
+          })
+          .where(eq(products.id, id))
+          .returning();
+
+        return {
+          success: `Product ${updateProduct[0].title} has been updated`,
+        };
+      }
+
+      const newProduct = await db
+        .insert(products)
+        .values({
+          description,
+          title,
+          price,
+        })
+        .returning();
+
+      return { success: `Product ${newProduct[0].title} has been created` };
+    } catch (err) {
+      return { error: "Error creating post" };
+    }
+  });
