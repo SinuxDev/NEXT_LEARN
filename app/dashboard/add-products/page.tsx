28b3af9988140222f7a08ad6c6a315d2c666d820
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-14 02:32:02 +0630

    create products forms

--- app/dashboard/add-products/page.tsx
+++ app/dashboard/add-products/page.tsx
@@ -1,7 +1,10 @@
-export default function AddProducts() {
-  return (
-    <div>
-      <h1>Add Products</h1>
-    </div>
-  );
+import ProductForm from "@/components/dashboard/products-form";
+import { auth } from "@/server/auth";
+import { redirect } from "next/navigation";
+
+export default async function AddProduct() {
+  const session = await auth();
+  if (session?.user.role !== "admin") return redirect("/dashboard/settings");
+
+  return <ProductForm />;
 }
---
+++ components/dashboard/products-form.tsx
@@ -0,0 +1,139 @@
+"use client";
+
+import { ProductSchema } from "@/types/types";
+import { useForm } from "react-hook-form";
+import z from "zod";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DollarSign } from "lucide-react";
+import Tiptap from "./tiptap";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAction } from "next-safe-action/hooks";
+import { createProducts } from "@/server/dashboard/create-products";
+
+export default function ProductForm() {
+  const form = useForm<z.infer<typeof ProductSchema>>({
+    resolver: zodResolver(ProductSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+      price: 0,
+    },
+    mode: "onChange",
+  });
+
+  const { execute, status } = useAction(createProducts, {
+    onSuccess(data) {
+      if (data.data?.error) {
+        console.log(data.data.error);
+      }
+
+      if (data.data?.success) {
+        console.log(data.data.success);
+      }
+    },
+    onError(err) {
+      console.error(err);
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof ProductSchema>) {
+    console.log(values);
+    execute(values);
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Add Your Product</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="SxHnin Stta" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Tiptap val={field.value} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Title</FormLabel>
+                  <FormControl>
+                    <div className="flex items-center gap-2">
+                      <DollarSign
+                        size={37}
+                        className="p-2 bg-muted rounded-md"
+                      />
+                      <Input
+                        {...field}
+                        type="number"
+                        placeholder="Your price in USD"
+                        step={0.1}
+                        min={0}
+                      />
+                    </div>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              disabled={
+                status === "executing" ||
+                !form.formState.isValid ||
+                !form.formState.isDirty
+              }
+              type="submit"
+            >
+              Submit
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
+}
