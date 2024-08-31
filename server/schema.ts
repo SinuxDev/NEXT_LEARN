e342290fd2ed661e630f3ebcbb37a0c8c8869bdb
Aung Ye Htet <aung.atg1738@gmail.com>
2024-08-31 18:18:14 +0630

    create: Orders Schema(Tables) & Config Drizzle

--- server/schema.ts
+++ server/schema.ts
@@ -133,6 +133,7 @@ export const VariantsImages = pgTable("images_variants", {
     .notNull()
     .references(() => productVariants.id, {
       onDelete: "cascade",
+      onUpdate: "cascade",
     }),
 });
 
@@ -217,4 +218,47 @@ export const reviewRelations = relations(reviews, ({ one }) => ({
 
 export const userRelations = relations(users, ({ many }) => ({
   reviews: many(reviews, { relationName: "user_reviews" }),
+  orders: many(orders, { relationName: "user_orders" }),
+}));
+
+export const orders = pgTable("orders", {
+  id: serial("id").primaryKey(),
+  userID: text("userID")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  total: real("total").notNull(),
+  status: text("status").notNull(),
+  created: timestamp("created").defaultNow(),
+  receiptURL: text("receiptURL"),
+});
+
+export const ordersRelations = relations(orders, ({ one, many }) => ({
+  user: one(users, {
+    fields: [orders.userID],
+    references: [users.id],
+    relationName: "user_orders",
+  }),
+  orderItems: many(orderProduct, { relationName: "orderProduct" }),
+}));
+
+export const orderProduct = pgTable("orderProduct", {
+  id: serial("id").primaryKey(),
+  quantity: integer("quantity").notNull(),
+  productVariantID: serial("productVariantID")
+    .notNull()
+    .references(() => productVariants.id, { onDelete: "cascade" }),
+  productID: serial("productID")
+    .notNull()
+    .references(() => products.id, { onDelete: "cascade" }),
+  orderID: integer("orderID")
+    .notNull()
+    .references(() => orders.id, { onDelete: "cascade" }),
+});
+
+export const orderProductRelations = relations(orderProduct, ({ one }) => ({
+  order: one(orders, {
+    fields: [orderProduct.orderID],
+    references: [orders.id],
+    relationName: "orderProduct",
+  }),
 }));
